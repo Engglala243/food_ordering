50950ec5681e0df0994a19ef5ffc3d37427d7ac5
oreximus <oreximus@gmail.com>
2024-11-18 17:53:12 +0530

    upgrading mysql connection with aiven cloud mysqldb

--- backEnd/utils/sqlFunctions.js
+++ backEnd/utils/sqlFunctions.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const config = require("../db/config");
 const { query } = require("express");
 const pool = mysql.createPool(config);
