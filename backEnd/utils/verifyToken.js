c57e5fafa6042a844a0608b031c6a14d83739062
oreximus <oreximus@gmail.com>
2024-11-21 07:40:43 +0530

    updating homepage layout, adding explore search bar

--- backEnd/utils/verifyToken.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  return (req, res, next) => {
-    const bearerToken = req.headers.authorization;
-    console.log(bearerToken);
-    if (!bearerToken) {
-    }
-    next();
-  };
-};
